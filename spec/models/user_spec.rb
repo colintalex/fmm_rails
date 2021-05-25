f8c3ab3e3a024cb652519cd0c27409cca73a4d31
Colin Alexander <colintalex@gmail.com>
2021-05-24 20:53:17 -0600

    Adds User validations and basic Model creation testing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,11 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  context "Required attributes" do
+  context "Validations" do
     it "is created with required attributes" do
       user = User.create!(name: 'Tester', email: 'test@test.com')
       expect(user).to be_valid
+      expect(user.name).to eql('Tester')
+      expect(user.email).to eql('test@test.com')
       expect(User.all.length).to eql(1)
+      user.destroy
+    end
+
+    it "is invalid without email" do
+      user = User.create(name: 'Tester')
+      expect(user).to_not be_valid
+      expect(User.all.length).to eql(0)
+    end
+
+    it "is invalid without name" do
+      user = User.create(email: 'tester@test.com')
+      expect(user).to_not be_valid
+      expect(User.all.length).to eql(0)
+    end
+
+    it "is invalid without name and email" do
+      user = User.create()
+      expect(user).to_not be_valid
+      expect(User.all.length).to eql(0)
     end
   end
 end
