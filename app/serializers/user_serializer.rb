4a16632123f0d5bb1c782771b4ed0299719151f8
Colin Alexander <colintalex@gmail.com>
2021-05-26 00:34:05 -0600

    Removes comments

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,10 +1,7 @@
 class UserSerializer
     include JSONAPI::Serializer
 
-    set_type :user  # optional
-    # set_id :owner_id # optional
+    set_type :user
     attributes :id, :name, :email, :favorites
     has_many :favorites
-    # belongs_to :owner, record_type: :user
-    # belongs_to :movie_type
 end
