5e92f1397a7ed16ea863b633565de555b2fcbc47
Colin Alexander <colintalex@gmail.com>
2021-05-26 00:41:14 -0600

    Adds default_location migration and optional validation for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
     validates :name, length: { minimum: 1 }, presence: true
     validates :email, uniqueness: true, presence: true
+    validates :default_location, presence: false
     has_secure_password
     has_many :favorites
 end
