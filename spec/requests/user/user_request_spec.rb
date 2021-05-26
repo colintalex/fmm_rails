863d0b620d9d6612b707763cd524891159ec714a
Colin Alexander <colintalex@gmail.com>
2021-05-26 00:07:29 -0600

    Adds specs for testing user attr updates

--- spec/requests/user/user_request_spec.rb
+++ spec/requests/user/user_request_spec.rb
@@ -30,7 +30,7 @@ RSpec.describe "Users", type: :request do
         expect(response).to have_http_status(400)
         resp = JSON.parse(response.body, :symbolize_names => true)
         expect(resp[:status]).to eql('error')
-        expect(resp[:messages]).to eql(["Name can't be blank"])
+        expect(resp[:messages]).to eql(["Name is too short (minimum is 1 character)", "Name can't be blank"])
         expect(resp).to_not have_key(:data)
       end
 
@@ -50,7 +50,7 @@ RSpec.describe "Users", type: :request do
         expect(response).to have_http_status(400)
         resp = JSON.parse(response.body, :symbolize_names => true)
         expect(resp[:status]).to eql('error')
-        expect(resp[:messages]).to eql(["Name can't be blank", "Email can't be blank"])
+        expect(resp[:messages]).to eql(["Name is too short (minimum is 1 character)", "Name can't be blank", "Email can't be blank"])
         expect(resp).to_not have_key(:data)
       end
 
@@ -69,10 +69,7 @@ RSpec.describe "Users", type: :request do
     context "Existing User" do
       before(:each) do
             @user = User.create!(name: 'Test', email: 'test@test.com', password: 'password')
-            payload = {email: 'test@test.com', password: 'password'}
-            post '/api/v1/auth/login', params: payload
-            @resp_init = JSON.parse(response.body, symbolize_names: true)
-            @auth = {'Authorization': "Bearer #{@resp_init[:token]}"}
+            @auth = {'Authorization': "Bearer #{JsonWebToken.encode(user_id: @user.id)}"}
       end
 
       it "successfully returns a single user" do
@@ -86,11 +83,44 @@ RSpec.describe "Users", type: :request do
         expect(resp[:data][:attributes][:email]).to eql(@user.email)
       end
 
-      it "returns an error for invalid user id" do
-        get "/api/v1/users/451231234123", headers: @auth
-        expect(response).to have_http_status(:not_found)
+      it "successfully updates a user and responds with updated user" do
+        payload = {
+          name: 'NewName'
+        }
+        put "/api/v1/users/#{@user.id}", headers: @auth, params: payload
+        expect(response).to have_http_status(:success)
         resp = JSON.parse(response.body, :symbolize_names => true)
-        expect(resp[:errors]).to eql('User not found')
+        expect(resp[:data][:id]).to eql(@user.id.to_s)
+        expect(resp[:data][:type]).to eql('user')
+        expect(resp[:data][:attributes][:id]).to eql(@user.id)
+        expect(resp[:data][:attributes][:name]).to eql(payload[:name])
+        expect(resp[:data][:attributes][:email]).to eql(@user.email)
+      end
+
+      it "returns an error updating with incorrect validations" do
+        payload = {
+          name: ''
+        }
+        put "/api/v1/users/#{@user.id}", headers: @auth, params: payload
+        expect(response).to have_http_status(:not_acceptable)
+        resp = JSON.parse(response.body, :symbolize_names => true)
+        expect(resp).to_not have_key(:data)
+        expect(resp).to have_key(:error)
+        expect(resp[:error]).to eql("Validation failed: Name is too short (minimum is 1 character), Name can't be blank")
+      end
+      
+    end
+    
+    describe "Deleting User" do
+      before(:each) do
+            @user = User.create!(name: 'Test', email: 'test@test.com', password: 'password')
+            @auth = {'Authorization': "Bearer #{JsonWebToken.encode(user_id: @user.id)}"}
+      end
+      it "can successfully delete an authorized user" do
+        delete "/api/v1/users/#{@user.id}", headers: @auth
+        expect(response).to have_http_status(:accepted)
+        resp = JSON.parse(response.body, :symbolize_names => true)
+        expect(resp[:message]).to eql('User successfully deleted')
       end
     end
   end
