6d987e8142419dcff87d9e103523d63cf42e3dcc
Colin Alexander <colintalex@gmail.com>
2021-05-25 18:47:04 -0600

    Refactors user controller for cleaner user handling

--- app/controllers/Api/v1/users_controller.rb
+++ app/controllers/Api/v1/users_controller.rb
@@ -2,19 +2,15 @@ class Api::V1::UsersController < ApplicationController
     before_action :authorize_request, except: :create
     before_action :find_user, except: %i[create index]
     def show
-        if @user.present?
-            render json: UserSerializer.new(@user)
-        else
-            render json: {status: "error", messages: ["Invalid User ID"]}, status: 400
-        end
+        render json: UserSerializer.new(@user)
     end
 
     def create
-        user = User.new(user_params)
-        if user.save
-            render json: UserSerializer.new(user)
+        @user = User.new(user_params)
+        if @user.save
+            render json: UserSerializer.new(@user)
         else
-            render json: {status: "error", messages: user.errors.full_messages}, status: 400
+            render json: {status: "error", messages: @user.errors.full_messages}, status: 400
         end
     end
 
