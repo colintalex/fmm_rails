0b16ec913342ab6521687a01a988fc6b6fb07278
Colin Alexander <colintalex@gmail.com>
2021-05-26 02:20:39 -0600

    Assigns token on User create

--- app/controllers/Api/v1/users_controller.rb
+++ app/controllers/Api/v1/users_controller.rb
@@ -10,7 +10,9 @@ class Api::V1::UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
-            render json: UserSerializer.new(@user)
+            token = JsonWebToken.encode(user_id: @user.id)
+            time = Time.now + 2.hours.to_i
+            render json: { token: token, exp: time.strftime("%m-%d-%Y %H:%M"), user: UserSerializer.new(@user)}
         else
             render json: {status: "error", messages: @user.errors.full_messages}, status: 400
         end
