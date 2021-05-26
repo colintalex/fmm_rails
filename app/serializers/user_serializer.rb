83899052c438038f4efd8bf551d3e138870cbafd
Colin Alexander <colintalex@gmail.com>
2021-05-26 00:08:01 -0600

    Updates UserSerializer to include relations

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -3,8 +3,8 @@ class UserSerializer
 
     set_type :user  # optional
     # set_id :owner_id # optional
-    attributes :id, :name, :email
-    # has_many :actors
+    attributes :id, :name, :email, :favorites
+    has_many :favorites
     # belongs_to :owner, record_type: :user
     # belongs_to :movie_type
 end
